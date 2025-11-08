5b72309ac75c4d639100392e65bf2385899f2bda
Genha <184705356+14-115-Grace@users.noreply.github.com>
2025-11-09 00:24:28 +0700

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = 'https://tmgkmaxgrxwibjegfome.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtZ2ttYXhncnh3aWJqZWdmb21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMzE3MDQsImV4cCI6MjA3NzgwNzcwNH0.ZQsHCQ2YU7ViCBG-pUSQ_40I8eCGbArPLmjAc1-D71Y';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
